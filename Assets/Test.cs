52b26541e46e8f80bd0a4698acd284b2e822d354
DESKTOP-358CSLP\USER <hs.jo.sw.p@gmail.com>
2024-02-13 11:56:42 +0900

    디버그

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("didi");
     }
 
     // Update is called once per frame
