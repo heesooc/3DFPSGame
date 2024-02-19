7d53874d8e1d94c2f5684238b3b1d78ac574448f
조희수 <hs.jo.sw.p@gmail.com>
2024-02-19 18:09:19 +0900

    드럼통 폭발할 때 주변 Hitable한 Monster와 Player에게 데미지 70

--- Assets/02.Scripts/Environment/Drum.cs
+++ Assets/02.Scripts/Environment/Drum.cs
@@ -31,7 +31,24 @@ public class Drum : MonoBehaviour, IHitable
             rigidbody.AddForce(Vector3.up * UpPower, ForceMode.Impulse);
             rigidbody.AddTorque(new Vector3(1, 0, 1) * UpPower / 2f); // 회전력
 
+            // 1. 터질 때
+            // private void OnCollisionEnter(Collision other) //두 물체 물리적 충돌 X ->드럼이 터지는 순간에 데미지를 입히는 코드 실행
+            {
+                // 2. 범위 안에 있는 모든 콜라이더를 찾는다.
+                int findLayer = LayerMask.GetMask("Player") | LayerMask.GetMask("Monster");
+                Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius, findLayer);
 
+                // 3. 찾은 콜라이더 중에서 타격 가능한(IHitable) 오브젝트를 찾아서 Hit()한다.
+                foreach (Collider collider in colliders)
+                {
+                    IHitable hitable = collider.GetComponent<IHitable>();
+                    if (collider.TryGetComponent<IHitable>(out hitable))
+                    // if (hitable != null)
+                    {
+                        hitable.Hit(Damage);
+                    }
+                }
+            }
 
         }
     }
@@ -50,24 +67,7 @@ public class Drum : MonoBehaviour, IHitable
         }
     }
 
-    // 1. 터질 때
-    private void OnCollisionEnter(Collision other)
-    {
-        // 2. 범위 안에 있는 모든 콜라이더를 찾는다.
-        int findLayer = LayerMask.GetMask("Player") |  LayerMask.GetMask("Monster");
-        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius, findLayer);
-
-        // 3. 찾은 콜라이더 중에서 타격 가능한(IHitable) 오브젝트를 찾아서 Hit()한다.
-        foreach (Collider collider in colliders)
-        {
-            IHitable hitable = collider.GetComponent<IHitable>();
-           if(collider.TryGetComponent<IHitable>(out hitable))
-            // if (hitable != null)
-            {
-                hitable.Hit(Damage);
-            }
-        }
-    }
+   
 
 }
 
